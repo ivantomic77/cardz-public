52925186312175ebaf3ecc12ca73260f242bfb16
ivan77tomic <ivan.tomic2108@gmail.com>
2024-11-23 21:15:28 +0100

    Added basic Go backend setup

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/FlamingoTP/cardz
+
+go 1.22.0
---
+++ backend/internal/handlers.go
@@ -0,0 +1,27 @@
+package internal
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type healthResponse struct {
+	Status string `json:"status"`
+}
+
+func GetHealth(w http.ResponseWriter, _ *http.Request) {
+	health := healthResponse{
+		Status: "UP",
+	}
+
+	response, err := json.Marshal(health)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
